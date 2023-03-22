4f594ce98190bf2e0b31099cf97850a23a7baf57
khasmir04 <kcaluscusao@gmail.com>
2023-03-22 22:19:53 +0800

    Improved styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,63 +12,125 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles.header}>
+        The KJ Store
+      </header>
       <main className={styles.main}>
+        <div className={styles.page}> Our Products</div>
         <div className={styles.grid}>
-          The KJ Store
-        </div>
-        <div className={styles.container}>
-
           <div className={styles.card}>
-            <Image alt='anya-image' src='/images/anya.jpg' width={200} height={200} />
-            <h2>Anya</h2>
-            <span>What is this</span>
-            <p>Description of the product here</p>
-            <button className={styles.buyBtn} onClick={() => {
-              checkout({
-                lineItems: [
-                  {
-                    price: 'price_1MoNx1F8wn5EaWfoDmEKGCRE',
-                    quantity: 1,
-                  }
-                ]
-              })
-            }}>Buy</button>
+            <div className={styles.cardContent}>
+              <Image alt='anya-image' src='/images/anya.jpg' width={200} height={200} />
+              <h2>Anya Toy</h2>
+              <p className={styles.price}>$10.00</p>
+              <p className={styles.desc}>Toy figure of Anya</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNx1F8wn5EaWfoDmEKGCRE',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
           </div>
-
-
           <div className={styles.card}>
-            <Image alt='damian-image' src='/images/damian.jpg' width={200} height={200} />
-            <h2>Damian</h2>
-            <span>What is this</span>
-            <p>Description of the product here</p>
-            <button className={styles.buyBtn} onClick={() => {
-              checkout({
-                lineItems: [
-                  {
-                    price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
-                    quantity: 1,
-                  }
-                ]
-              })
-            }}>Buy</button>
+            <div className={styles.cardContent}>
+              <Image alt='damian-image' src='/images/damian.jpg' width={200} height={200} />
+              <h2>Damian Toy</h2>
+              <p className={styles.price}>$1.00</p>
+              <p className={styles.desc}>Toy figure of Damian</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
           </div>
-
-
           <div className={styles.card}>
-            <h2>Title here</h2>
-            <span>What is this</span>
-            <p>Description of the product here</p>
+            <div className={styles.cardContent}>
+              <Image alt='no image available' src='/images/noimage.jpg' width={200} height={200} />
+              <h2>Damian Toy</h2>
+              <p className={styles.price}>$50.00</p>
+              <p className={styles.desc}>Description of product goes here</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
           </div>
-
-
           <div className={styles.card}>
-            <h2>Title here</h2>
-            <span>What is this</span>
-            <p>Description of the product here</p>
+            <div className={styles.cardContent}>
+              <Image alt='no image available' src='/images/noimage.jpg' width={200} height={200} />
+              <h2>Damian Toy</h2>
+              <p className={styles.price}>$99.00</p>
+              <p className={styles.desc}>Description of product goes here</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
           </div>
-
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <Image alt='no image available' src='/images/noimage.jpg' width={200} height={200} />
+              <h2>Damian Toy</h2>
+              <p className={styles.price}>$99.00</p>
+              <p className={styles.desc}>Description of product goes here</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <Image alt='no image available' src='/images/noimage.jpg' width={200} height={200} />
+              <h2>Damian Toy</h2>
+              <p className={styles.price}>$99.00</p>
+              <p className={styles.desc}>Description of product goes here</p>
+              <button className={styles.buyBtn} onClick={() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: 'price_1MoNxzF8wn5EaWfoLVAxyLHT',
+                      quantity: 1,
+                    }
+                  ]
+                })
+              }}>Buy</button>
+            </div>
+          </div>
         </div>
       </main>
+      <footer className={styles.footer}>
+        Dev KJ
+      </footer>
     </>
   )
 }
